f0f97c8d020d970d21720136db79d02b8093dcbd
Anissa3005 <anissa.chadouli@gmail.com>
2023-06-12 11:59:38 +0900

    feat: POST req  user email and id by mail

--- backend/src/controller/login.controller.ts
+++ backend/src/controller/login.controller.ts
@@ -3,7 +3,7 @@ import express, {Request, Response} from "express";
 import loginService from "../service/login.service";
 
 export default {
-    async updateAccessTokenLogin (req: Request, res: Response){
+    async updateAccessTokenLogin(req: Request, res: Response){
         try {
             const paramsId = req.params.id
             const userId = parseInt(paramsId)
@@ -11,6 +11,19 @@ export default {
             res.status(200).send(newAccessToken);
         } catch (error) {
             res.status(304).send("NOT MODEFIED");
+        }
+    },
+
+    async getUserData(req: Request, res:Response){
+        try {
+            const userData = await loginService.getUserData(req.body);
+            if(userData) {
+                res.status(200).send(userData)
+            } else {
+                res.status(400).send("User does not exist")
+            }
+        } catch (error: any) {
+            console.error(error);
         }
     }
 }
--- backend/src/service/login.service.ts
+++ backend/src/service/login.service.ts
@@ -4,9 +4,18 @@ type Token = {
     access_token: string
 }
 
+type Email = {
+    email: string
+}
+
 export default  {
     updateAccessTokenLogin(userId:number, body: Token) {
         const {access_token: token} = body;
         return loginModel.updateAccessTokenLogin(userId, token);
+    },
+
+    getUserData(userEmail: Email) {
+        const {email} = userEmail;
+        return loginModel.getUserData(email);
     }
 }
