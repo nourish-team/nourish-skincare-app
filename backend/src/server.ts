0278ae4866ff5dac0b405479e452f151e0bdc7eb
Anissa3005 <anissa.chadouli@gmail.com>
2023-06-12 17:29:35 +0900

    feat: Cors added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,20 @@
 import express, { Application, Router } from 'express';
 import router from './routes';
+import cors from 'cors';
 const PORT: number = 8080
 
 const app: Application = express()
+
+const allowedOrigins = [
+    "http://10.0.2.2:8080"
+];
+
+const options: cors.CorsOptions = {
+    origin: allowedOrigins
+  };
+
+
+app.use(cors(options));
 
 app.use(express.json());
 app.use(router);
