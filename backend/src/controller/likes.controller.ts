d55d53541bd8a494a7f300a0301078cb09e1e0d8
Anissa3005 <anissa.chadouli@gmail.com>
2023-06-12 16:37:04 +0900

    feat: Get likes works

--- backend/src/controller/likes.controller.ts
+++ backend/src/controller/likes.controller.ts
@@ -10,7 +10,16 @@ export default {
             console.error(error);
             res.status(400).send("Can't create like in database");
         }
-       
+    },
 
+    async getTotalLikes(req: Request, res: Response) {
+        try {
+            const routineId =  req.params.routineid;
+            const totalLikes = await likesService.getTotalLikes(routineId);
+            res.status(200).send(totalLikes);
+        } catch (error) {
+            console.error(error);
+            res.status(400).send("Not found");
+        }
     }
 }
