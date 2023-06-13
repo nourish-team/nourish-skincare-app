847c652cadcc09476eda49fb5819e8cc1eb45fad
Anissa3005 <anissa.chadouli@gmail.com>
2023-06-13 15:20:49 +0900

    feat: basic post req for post

--- frontend/src/screens/SkinTypeScreen.tsx
+++ frontend/src/screens/SkinTypeScreen.tsx
@@ -1,22 +1,28 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import { View, Text, StyleSheet, Button, Pressable } from "react-native";
 import { ParamListBase, useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import UserContext from "../contexts/UserContext";
 
 const SkincareTypeScreen: React.FC<{ route: any }> = ({ route }) => {
   const { skincareType } = route.params;
   const [routinesByType, setRoutinesByType] = useState([]);
   const [fetchRoutinesError, setFetchRoutinesError] = useState(false);
-
+  const {userId} = useContext(UserContext);
+  
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
 
   const handleBackPress = () => {
     navigation.navigate("HomeScreen");
   };
 
+  useEffect(() => {
+    fetchRoutinesByType();
+  }, []);
+
   const fetchRoutinesByType = async () => {
     try {
-      const response = await fetch(""); // put in route later
+      const response = await fetch(`http://10.0.2.2:8080/routine/skintype/${skincareType.toLowerCase()}`); // put in route later
       const data = await response.json();
       setRoutinesByType(data);
     } catch (error) {
@@ -24,12 +30,45 @@ const SkincareTypeScreen: React.FC<{ route: any }> = ({ route }) => {
     }
   };
 
+  const handelPostLike = async (routineId:number) => {
+    console.log(routineId, "was clicked clicked");
+    const postReq = {
+      users_id: userId,
+      routines_id: routineId,
+      like: true
+    }
+
+    try {
+      const response = await fetch(`http://10.0.2.2:8080/like/routine`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postReq),
+      }); // put in route later
+      const data = await response.json();
+      console.log("data of like", data);
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>You selected {skincareType} skin type</Text>
-      <Button title="Back" onPress={handleBackPress} />
-    </View>
-    // Map through the routines here later
+      {routinesByType.map((routine: any) => (
+        <View style={styles.routine} key={routine.id}>
+          <Text>{routine.routine_name}</Text>
+          <Text>{routine.routine_product}</Text>
+          <Text>{routine.created_at}</Text>
+          <Pressable style={styles.button} onPress={() => handelPostLike(routine.id)}>
+            <Text>Like</Text>
+          </Pressable>
+          
+        </View>
+      ))}
+      <Button title="Back" onPress={handleBackPress} />  
+    </View>  
   );
 };
 
@@ -41,6 +80,13 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     paddingRight: 20,
   },
+  routine: {
+    marginBottom: 20
+  },
+  button: {
+      width: 60,
+      backgroundColor: "#FFB6C1",
+  }
 });
 
 export default SkincareTypeScreen;
