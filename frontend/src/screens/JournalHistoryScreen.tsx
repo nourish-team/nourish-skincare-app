e24284f0857a3f35ef9d03bfe2da1362cc0e7b92
shonancanuck <shonanxspark@gmail.com>
2023-06-14 16:21:08 +0900

    feat: add scrolling to journal history

--- frontend/src/screens/JournalHistoryScreen.tsx
+++ frontend/src/screens/JournalHistoryScreen.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+} from "react-native";
 import UserContext from "../contexts/UserContext";
 import { RouteProp } from "@react-navigation/native";
 import { RootStackParamList } from "../navigation/types";
@@ -14,37 +21,34 @@ type Props = {
   route: JournalHistoryScreenRouteProp;
 };
 
+type ItemProps = {
+  comments: string | undefined;
+  date: string;
+  img_url: string | undefined;
+  routine_id: {
+    routine_name: string;
+    routine_product: number[];
+    skin_type: string;
+  };
+};
+
 const JournalHistoryScreen: React.FC<Props> = ({ route }) => {
   const { routineId } = route.params;
   const { userId } = useContext(UserContext);
-  const [journalEntries, setJournalEntries] = useState<
-    {
-      comments: string | undefined;
-      date: string;
-      img_url: string | undefined;
-      routine_id: {
-        routine_name: string;
-        routine_product: number[];
-        skin_type: string;
-      };
-    }[]
-  >([]);
-  const [display, setDisplay] = useState<JSX.Element[]>([]);
+  const [journalEntries, setJournalEntries] = useState<ItemProps[]>([]);
 
-  const displayData = () => {
-    return journalEntries.map((entry, index) => (
-      <View key={index}>
-        <Text>{entry.date}</Text>
-        <Text>{entry.comments}</Text>
-        {entry.img_url && (
-          <Image
-            source={{ uri: entry.img_url }}
-            style={{ width: 100, height: 100 }}
-          ></Image>
-        )}
-      </View>
-    ));
-  };
+  const Item = ({ comments, date, img_url, routine_id }: ItemProps) => (
+    <View style={styles.item}>
+      <Text>{date}</Text>
+      <Text>{comments}</Text>
+      {img_url && (
+        <Image
+          source={{ uri: img_url }}
+          style={{ width: 100, height: 100 }}
+        ></Image>
+      )}
+    </View>
+  );
 
   useEffect(() => {
     const getHistory = async () => {
@@ -63,14 +67,24 @@ const JournalHistoryScreen: React.FC<Props> = ({ route }) => {
 
   useEffect(() => {
     if (journalEntries.length > 0) {
-      setDisplay(displayData());
+      // setDisplay(displayData());
     }
   }, [journalEntries]);
 
   return (
     <View style={styles.container}>
       <Text>Journal History {routineId}</Text>
-      <View>{display}</View>
+      <FlatList
+        data={journalEntries}
+        renderItem={({ item }) => (
+          <Item
+            date={item.date}
+            comments={item.comments}
+            img_url={item.img_url}
+            routine_id={item.routine_id}
+          />
+        )}
+      />
     </View>
   );
 };
@@ -82,6 +96,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFDD0",
     paddingLeft: 20,
     paddingRight: 20,
+  },
+
+  item: {
+    flex: 1,
+    backgroundColor: "#FFFDD0",
+    margin: 5,
   },
 });
 
